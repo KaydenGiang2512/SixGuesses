199eaaeace92fe9ba34cb3412121908bb276a4d5
KaydenGiang2512 <nhatkhanhgiang2512@gmail.com>
2022-08-18 12:52:31 +0700

    Initial Commit

---
+++ SixGuesses/SixGuessesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixGuessesApp.swift
+//  SixGuesses
+//
+//  Created by Khanh Giang Nhat on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SixGuessesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
