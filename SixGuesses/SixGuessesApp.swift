8b62a2692b3b1ca85fa2618d20d3039ea86b8e92
Giang Nhật Khánh (Kayden) <73809007+KaydenGiang2512@users.noreply.github.com>
2022-09-04 12:52:41 +0700

    added header section at source file

--- SixGuesses/SixGuessesApp.swift
+++ SixGuesses/SixGuessesApp.swift
@@ -1,15 +1,19 @@
-//
-//  SixGuessesApp.swift
-//  SixGuesses
-//
-//  Created by Khanh Giang Nhat on 18/08/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Giang Nhat Khanh (Kayden)
+  ID: s3878182
+  Created  date: 08/08/2022 (e.g. 31/07/2022)
+  Last modified: 07/09/2022 (e.g. 05/08/2022)
+  Acknowledgement: YouTube  Tutorials, raywenderlich.com 
+*/
 
 import SwiftUI
 
 @main
 struct SixGuessesApp: App {
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
