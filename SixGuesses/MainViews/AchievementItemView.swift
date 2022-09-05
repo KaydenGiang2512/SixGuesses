e31e10795b203c3423674061fbf9e27262182460
KaydenGiang2512 <nhatkhanhgiang2512@gmail.com>
2022-09-05 15:05:33 +0700

    added Achievement Views

---
+++ SixGuesses/MainViews/AchievementItemView.swift
@@ -0,0 +1,20 @@
+//
+//  AchievementItemView.swift
+//  SixGuesses
+//
+//  Created by Khanh Giang Nhat on 05/09/2022.
+//
+
+import SwiftUI
+
+struct AchievementItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AchievementItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AchievementItemView()
+    }
+}
