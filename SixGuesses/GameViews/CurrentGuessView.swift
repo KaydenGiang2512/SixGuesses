90a8ece983ea9ac871cd94a5d99b2077fa6fc729
Giang Nhật Khánh (Kayden) <73809007+KaydenGiang2512@users.noreply.github.com>
2022-09-05 22:05:55 +0700

    added comments

--- SixGuesses/GameViews/CurrentGuessView.swift
+++ SixGuesses/GameViews/CurrentGuessView.swift
@@ -8,25 +8,34 @@
 import SwiftUI
 
 struct CurrentGuessView: View {
+    
+    // Declaring the initial variables
     @Binding var guess: WordGuess
     var wordLength: Int
 
     var unguessedLetters: Int {
       wordLength - guess.word.count
     }
+    
     @State var shakeOffset = 0.0
 
 
     var body: some View {
-        // 1
+        
+        // Wrap the whole view with Geometry Reader to access the size of the view via the proxy
         GeometryReader { proxy in
           HStack {
             Spacer()
-            // 2
+              
+            // Calculating the width of each letter with respect to the entire view width
             let width = (proxy.size.width - 40) / 5 * 0.8
-            // 3
+              
+            // Using a for-loop to go through each letter in the guess along with specifying an id,
+            // since each letter in the word is unique
             ForEach(guess.word.indices, id: \.self) { index in
-              // 4
+                                                     
+              // 4 Assigning each letter at each individual index to a letter constant,
+              // then use the GuessView to display them in a neat layout from left to right
               let letter = guess.word[index]
               GuessView(letter: letter, size: width, index: index)
                     .accessibilityLabel(
@@ -34,27 +43,33 @@ struct CurrentGuessView: View {
                     )
 
             }
-            // 5
+              
+            // Using the EmptyView to indicate the unused slots (unguessed letters)
             ForEach(0..<unguessedLetters, id: \.self) { _ in
               EmptyView(size: width)
             }
             Spacer()
           }
             .padding(5.0)
-            // 1
+                        
+            // Applying an offset value to the view
             .offset(x: shakeOffset)
-            // 2
+                        
+            // Telling SwiftUI to monitor any changes in the guess status to perform the animation
             .onChange(of: guess.status) { newValue in
-            // 3
+                                         
+            // In the case of an invalid word, we use the withAnimation function to animate the occurance
                 if newValue == .invalidWord {
                     withAnimation(.linear(duration: 0.1).repeatCount(3)) {
                         shakeOffset = -15.0
                     }
-                // 4
+                    
+                // Applying a 3-second delay before resetting the position of the view to its original spot
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
                         withAnimation(.linear(duration: 0.1).repeatCount(3)) {
                             shakeOffset = 0.0
-                            // 5
+                            
+                            // Wait for 1 additional second before setting the guess status back to pending
                             DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                                 guess.status = .pending
                             }
@@ -62,6 +77,9 @@ struct CurrentGuessView: View {
                     }
                 }
             }
+                        
+          // Adding an overlay text field that will only be invoked when the player has submitted an unrecognized word,
+          // before dismissing the guess and set the guess status to pending
           .overlay(
             Group {
               if guess.status == .invalidWord {
