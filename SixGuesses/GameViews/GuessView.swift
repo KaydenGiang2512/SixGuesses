277854f6a252c10cbdfd0385667a687e2eb147fd
Giang Nhật Khánh (Kayden) <73809007+KaydenGiang2512@users.noreply.github.com>
2022-09-05 22:22:54 +0700

    added comments

--- SixGuesses/GameViews/GuessView.swift
+++ SixGuesses/GameViews/GuessView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct GuessView: View {
+  
+  // Declaring the initial variables
   var letter: GuessedLetter
   var size: Double
   var index: Int
@@ -19,22 +21,32 @@ struct GuessView: View {
             axis: (x: 0.0, y: 1.0, z: 0.0)
           )
           .font(.title)
-          // 1
+    
+          // Setting the font's foreground color to the system background,
+          // allowing for easier color scheme adaptability
           .foregroundColor(Color(UIColor.systemBackground))
-          // 2
+    
+          // Setting the frame by width and height of the passed value
           .frame(width: size, height: size)
-          // 3
+    
+          // Setting the background color of each letter box to its corresponding status color
           .background(letter.statusColor)
-          // 4
+    
+          // Adding the corner radius property to give the tile a smooth and polished aesthetic
           .cornerRadius(size / 5.0)
-            // 1
+    
+            // Using the rotation3DEffect to flexibly adjust the axis of the animation plane
           .rotation3DEffect(
-            // 2
+            
+            // Unless the letter status is unknown, rotate that letter tile by 180 degrees
             .degrees(letter.status == .unknown ? 0 : 180),
-            // 3
+            
+            // Specifying the rotation axis throughout this animation (y-axis in this case)
             axis: (x: 0.0, y: 1.0, z: 0.0)
           )
-          // 4
+    
+          // Prompting SwiftUI to animate this view on any changes occuring on the letter status,
+          // and ensures that it is happening at constant speed from start to finish
           .animation(
             .linear(duration: 1.0).delay(0.1 * Double(index)),
             value: letter.status
