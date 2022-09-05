b66c207d1ce1b91283bd752f61474858e1ec47f9
Giang Nhật Khánh (Kayden) <73809007+KaydenGiang2512@users.noreply.github.com>
2022-09-05 17:18:27 +0700

    added comments

--- SixGuesses/GameData/AppColorScheme.swift
+++ SixGuesses/GameData/AppColorScheme.swift
@@ -7,10 +7,12 @@
 
 import SwiftUI
 
+// Creating an enum containing the different schemes that the application will toggle between
 enum ColorScheme: Int {
     case unspecified, light, dark
 }
 
+// Creating a class to implement the different schemes as well as storing it in UserDefaults
 class AppColorScheme: ObservableObject {
     @AppStorage("colorScheme") var colorScheme: ColorScheme = .unspecified {
         didSet {
@@ -18,6 +20,7 @@ class AppColorScheme: ObservableObject {
         }
     }
     
+    //This function sets the color scheme according to user input, thereby overriding the default styles
     func applyColorScheme() {
         UIWindow.key?.overrideUserInterfaceStyle = UIUserInterfaceStyle(rawValue: colorScheme.rawValue)!
     }
