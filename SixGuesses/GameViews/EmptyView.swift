d730e60ad0f2991cfae6febd914c1ae4e90df0d6
Giang Nhật Khánh (Kayden) <73809007+KaydenGiang2512@users.noreply.github.com>
2022-09-05 22:07:46 +0700

    added comments

--- SixGuesses/GameViews/EmptyView.swift
+++ SixGuesses/GameViews/EmptyView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct EmptyView: View {
+  
+  // Declaring the initial variable
   var size: Double
 
   var body: some View {
+    
+    // Creating a rounded rectangle shape, which will become the box for each letter
     RoundedRectangle(cornerRadius: size / 5.0)
       .stroke(Color(UIColor.label))
       .frame(width: size, height: size)
