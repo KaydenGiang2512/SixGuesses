305a8c3074918bd5012d6e22d6cf3c9d3eccf537
Giang Nhật Khánh (Kayden) <73809007+KaydenGiang2512@users.noreply.github.com>
2022-09-06 10:44:01 +0700

    added comments

--- SixGuesses/MainViews/SettingsView.swift
+++ SixGuesses/MainViews/SettingsView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    // Declaring the initial variables
     @EnvironmentObject var csManager: AppColorScheme
     @EnvironmentObject var game: SixGuesses
     @Environment(\.dismiss) var dismiss
@@ -15,7 +17,10 @@ struct SettingsView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Section(footer: Text("When Hard Mode is enabled, only the correctly placed letters will be indicated!")
+                
+                // Creating a section element to add a footer to the Hard Mode toggle button,
+                // to briefly explain how Hard Mode will work and how to beat it
+                Section(footer: Text("When Hard Mode is enabled, only the correctly placed letters/not-in-word letters will be indicated! (Green or Gray only)")
                         .foregroundColor(.gray.opacity(0.75))) {
                     VStack(alignment: .leading) {
                         Toggle("Hard Mode", isOn: $game.hardMode)
@@ -23,6 +28,9 @@ struct SettingsView: View {
                     }
                 }
                 .padding(.vertical)
+                
+                // Creating a picker element with 3 options to toggle between light mode,
+                // dark mode or based on the default system preferences
                 Text("Toggle App Themes")
                 Picker("Display mode", selection: $csManager.colorScheme) {
                     Text("Light").tag(ColorScheme.light)
