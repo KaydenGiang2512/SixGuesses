503f326e02dacbfc184d69ed953c0cedf0682fe4
Giang Nhật Khánh (Kayden) <73809007+KaydenGiang2512@users.noreply.github.com>
2022-09-06 20:54:38 +0700

    added header section

--- SixGuesses/SixGuessesApp.swift
+++ SixGuesses/SixGuessesApp.swift
@@ -5,9 +5,9 @@
   Assessment: Assignment 2
   Author: Giang Nhat Khanh (Kayden)
   ID: s3878182
-  Created  date: 08/08/2022 (e.g. 31/07/2022)
-  Last modified: 07/09/2022 (e.g. 05/08/2022)
-  Acknowledgement: YouTube  Tutorials, raywenderlich.com 
+  Created  date: 08/08/2022
+  Last modified: 07/09/2022
+  Acknowledgement: YouTube  Tutorials, RMIT University Lecture Modules of COSC2659 provided by my lecturer, Mr. Tom Huynh
 */
 
 import SwiftUI
